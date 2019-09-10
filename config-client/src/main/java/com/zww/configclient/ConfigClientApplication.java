cc7a5b7dfc4b9c00fe3b05c38b539be832626a00
zhaoweiwei <zhaoweiwei@shein.com>
2019-09-10 23:36:30 +0800

    config

--- config-client/src/main/java/com/zww/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/zww/configclient/ConfigClientApplication.java
@@ -2,12 +2,20 @@ package com.zww.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 客户端
+ *
+ * @author zhaoww
+ * @since 2019/9/10
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
 
 }
--- config-client/src/main/java/com/zww/configclient/controller/HelloController.java
+++ config-client/src/main/java/com/zww/configclient/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.zww.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zhaoww
  * @since 2019/09/10
  */
+@RefreshScope
 @RestController
 public class HelloController {
 
